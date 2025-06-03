3a6b9600f7be74070779670422744f495c34ba47
Yuliia Palamar <67055220+yuliiapalamar@users.noreply.github.com>
2025-06-04 00:16:23 +0300

    Update fashion_multitask_model.py

--- fashion_multitask_model.py
+++ fashion_multitask_model.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader, random_split
 
 # Параметри
 DATA_DIR = 'dataset'
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 NUM_EPOCHS = 10
 IMAGE_SIZE = 224
 MODEL_PATH = 'model.pt'
